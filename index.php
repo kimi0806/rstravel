c720dc99d617c6796f2d175459bdebcf735da3b4
Romel Sallutal <kimi@Romels-Air.home>
2021-08-07 09:26:43 +0200

    test

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        This is WCA romel page
+    </head>
+<body>
+    <nav>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+
+    </nav>
+</body>
+</html>
